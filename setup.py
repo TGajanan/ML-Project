a25f18c8e217a30e40678d04fccaac3623bfe982
Tgajanan <gajanantodetti007@gmail.com>
2023-04-09 22:10:38 +0530

    logging and exception

--- setup.py
+++ setup.py
@@ -2,20 +2,17 @@ from setuptools import find_packages,setup
 from typing import List
 
 
-hypen_e_dot='-e .'
+hypen_e_dot='-e .' #this line for removeing ingoring the -e
 def get_requirements(file_path:str)->List[str]:
     '''This function will return the all requirements'''
     requirements=[]
     with open(file_path) as file_obj:
         requirements=file_obj.readlines()
-        requirements=[req.replace('\n','') for req in requirements]
+        requirements=[req.replace('\n','') for req in requirements]  #for replacing the \
         
-        if hypen_e_dot in requirements:
-            requirements.remove(hypen_e_dot)
-
-
+        if hypen_e_dot in requirements:    
+            requirements.remove(hypen_e_dot) #To ignoring the -e
     return requirements
-
 
 
 setup(
@@ -23,6 +20,6 @@ setup(
     version='0.0.1',
     author='Gajanan',
     Author_email='gajanantodetti1998@gmail.com',
-    packages=find_packages(),
-    install_requires=get_requirements('requirements.txt')
+    packages=find_packages(), 
+    install_requires=get_requirements('requirements.txt')  #to install libraries 
 )
